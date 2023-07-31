80bbe6db6dce2bdea98a21c0ff8837378a5da87e
mahcodes <mhmdali102@bk.ru>
2023-07-31 12:27:52 +0300

    post tags

--- src/routes/feed/Tags.tsx
+++ src/routes/feed/Tags.tsx
@@ -1,11 +1,41 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import useAxios from "../../hooks/useAxios";
+import { Chip } from "@mui/material";
 
 interface ITagsProps {
   ids: number[];
 }
 
+interface ITags {
+  [id: string]: string;
+}
+
 const Tags: FC<ITagsProps> = ({ ids }) => {
-  return ids.map((id) => <span key={id}>{id}, </span>);
+  const [tags, setTags] = useState<ITags>({});
+  const { sendItBaby, response } = useAxios();
+
+  useEffect(() => {
+    (async () => {
+      ids.forEach((id) => {
+        sendItBaby({
+          url: `/Tags/${id}`,
+        });
+      });
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ids]);
+
+  useEffect(() => {
+    if (!response?.data.name) return;
+    setTags((current) => ({
+      ...current,
+      [response?.data.id]: response?.data.name,
+    }));
+  }, [response]);
+
+  return Object.entries(tags).map(([k, v]) => (
+    <Chip key={k} label={v} sx={{ mr: 1 }} />
+  ));
 };
 
 export default Tags;

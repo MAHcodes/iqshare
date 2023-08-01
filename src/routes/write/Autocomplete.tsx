5c6ed8ff12dc37e07ffe89fe5a7e27d61ec7b745
mahcodes <mhmdali102@bk.ru>
2023-08-01 10:06:45 +0300

    new bug

--- src/routes/write/Autocomplete.tsx
+++ src/routes/write/Autocomplete.tsx
@@ -3,8 +3,8 @@ import { FormikValues } from "formik";
 import { FC, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
-import axios from "axios";
-import { DEFAULT_CONFIG } from "../../config/axios";
+import useAxios from "../../hooks/useAxios";
+import { IPost } from "../feed/Post";
 
 interface IAutocompleteFormProps {
   formik: FormikValues;
@@ -17,42 +17,67 @@ interface Tag {
 
 const AutocompleteForm: FC<IAutocompleteFormProps> = ({ formik }) => {
   const userId = useSelector((state: RootState) => state.user.id);
+  const { sendItBaby, response } = useAxios();
+  const [posts, setPosts] = useState<IPost[]>([]);
   const [tags, setTags] = useState<Tag[]>([]);
 
   useEffect(() => {
+    sendItBaby({
+      url: `/Users/${userId}`,
+    });
+    // (async () => {
+    //   await axios.request({
+    //     ...DEFAULT_CONFIG,
+    //     url: `/Users/${userId}`,
+    //   });
+    //   setPosts(response?.data?.posts);
+    // const tagIds = new Set();
+    // console.log({ posts });
+    // posts?.forEach((post: { tagIds: number[] }) => {
+    //   post.tagIds.forEach((tag) => {
+    //     tagIds.add(tag);
+    //   });
+    // });
+    // console.log({ tagIds });
+    // tagIds.forEach((tagId) => {
+    //   (async () => {
+    //     const response = await axios.request({
+    //       ...DEFAULT_CONFIG,
+    //       url: `/Tags/${tagId}`,
+    //     });
+    //     console.log({ response });
+    //     setTags((current: Tag[]) => [
+    //       ...current,
+    //       {
+    //         name: response.data.name,
+    //         id: response.data.id,
+    //       },
+    //     ]);
+    //   })();
+    // });
+    // })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId, setTags]);
+
+  useEffect(() => {
+    if (response?.data.posts) {
+      setPosts(response?.data.posts);
+    }
+  }, [response]);
+
+  useEffect(() => {
+    console.log(posts);
     (async () => {
-      const response = await axios.request({
-        ...DEFAULT_CONFIG,
-        url: `/Users/${userId}`,
-      });
-      console.log(response);
-      const posts = response?.data?.posts;
       const tagIds = new Set();
-      console.log({ posts });
-      posts?.forEach((post: { tagIds: number[] }) => {
+      posts.forEach((post: { tagIds: number[] }) => {
         post.tagIds.forEach((tag) => {
           tagIds.add(tag);
+          console.log(tag);
         });
       });
       console.log({ tagIds });
-      tagIds.forEach((tagId) => {
-        (async () => {
-          const response = await axios.request({
-            ...DEFAULT_CONFIG,
-            url: `/Tags/${tagId}`,
-          });
-          console.log({ response });
-          setTags((current: Tag[]) => [
-            ...current,
-            {
-              name: response.data.name,
-              id: response.data.id,
-            },
-          ]);
-        })();
-      });
     })();
-  }, [userId, setTags]);
+  }, [posts]);
 
   useEffect(() => {
     console.log(tags);
@@ -75,7 +100,7 @@ const AutocompleteForm: FC<IAutocompleteFormProps> = ({ formik }) => {
       freeSolo
       multiple
       value={formik.values.tags}
-      onChange={(e, newValue) => {
+      onChange={(_, newValue) => {
         // handleNewTag(e.target.value);
         formik.setFieldValue("tags", newValue);
       }}
